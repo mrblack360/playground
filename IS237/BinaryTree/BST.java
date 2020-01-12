02ac5fc21dccb6075fbb431506b133542e957f54
Maswi Mussa R <49749184+mrblack360@users.noreply.github.com>
2020-01-12 22:42:54 +0300

    Add files via upload

--- IS237/BinaryTree/BST.java
+++ IS237/BinaryTree/BST.java
@@ -1,1 +1,40 @@
+package BinaryTree;
 
+public class BST {
+    int key;
+    BST leftChild;
+    BST rightChild;
+
+    public BST(int key, BST leftChild, BST rightChild) {
+        this.key = key;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public int getData(BST bst) {
+        return bst.key;
+    }
+
+    public int getLeftData(BST bst) {
+        BST leftChild = bst.leftChild;
+        return leftChild.key;
+    }
+
+    public int getRightChild(BST bst) {
+        BST rightChild = bst.rightChild;
+        return rightChild.key;
+    }
+
+    public void setData(int data) {
+        this.key = data;
+    }
+
+    public void setLeftData(BST leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setRightData(BST rightChild) {
+        this.rightChild = rightChild;
+    }
+
+}
