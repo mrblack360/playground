f93cb5eab4107287fecff0b5ae173259bd4edf68
Maswi, MR <rmaswi360@outlook.com>
2022-04-07 19:15:25 +0300

    Updated logger style

--- FileProcessor/FileProcessor.java
+++ FileProcessor/FileProcessor.java
@@ -1,3 +1,5 @@
+// Written by Maswi<rmaswi360@outlook.com> on April 7, 2022 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -5,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,34 +21,35 @@ public class FileProcessor{
     File folder = new File(filesUrl.getPath());
     public static void main(String[] args) {
         FileProcessor fileProcessor = new FileProcessor();
+        Date sasa = new Date();
         File fileContents;
         FileWriter export;
         Scanner contentReader;
+        PrintWriter mwandishi=null;
         Pattern IPPattern=Pattern.compile("172\\.27\\.[0-9]+\\.[0-9]+");
         Pattern MSISDNPattern = Pattern.compile("255\\d{9,12}");
-        String MSISDN="";String IP="";
+        String MSISDN="";
+        String IP="";
 
+        System.out.println(sasa.toString()+": Starting Script...");
         try{
-            // System.out.println(fileProcessor.exportsUrl.getPath());
-            File exportFile=new File("/home/zalongwa/Code/Private/playground/FileProcessor/exports/export1.csv");
+            File exportFile=new File(fileProcessor.exportsUrl.getPath()+"/exports"+fileProcessor.dateTime+".csv");
             if(exportFile.createNewFile()){
-                // System.out.println("Creating export file...");
+                System.out.println(sasa.toString()+": Creating export file...");
             }else{
-                // System.out.println("Export file is available...");
+                System.out.println(sasa.toString()+": Export file is available...");
             }
-            export = new FileWriter("/home/zalongwa/Code/Private/playground/FileProcessor/exports/export1.csv");
-            PrintWriter mwandishi = new PrintWriter(export);
+            export = new FileWriter(fileProcessor.exportsUrl.getPath()+"/exports"+fileProcessor.dateTime+".csv");
+            mwandishi = new PrintWriter(export);
             mwandishi.println("MSISDN,IP_ADDRESS");
-            System.out.println("MSISDN,IP_ADDRESS");
-            // export.append("MSISDN,IP_ADDRESS");
-            // export.write("MSISDN,IP_ADDRESS");
-            // export.write("Yy");
+            System.out.println(sasa.toString()+": Appending Headings...");
         }catch(Exception e){
             e.printStackTrace();
         }
-
+        System.out.println(sasa.toString()+": Reading available files...");
         for (final File file: fileProcessor.folder.listFiles()){
             if(file.isFile()){
+            System.out.println(sasa.toString()+": Processing file "+file.getName());
                 try{fileContents = new File(fileProcessor.filesUrl.getPath()+file.getName());
                 contentReader = new Scanner(fileContents);
                 while(contentReader.hasNextLine()){
@@ -63,7 +67,7 @@ public class FileProcessor{
                         }
                     }
                     if(MSISDN.length()>3 && IP.length()>3){
-                        System.out.println(MSISDN+","+IP);
+                        mwandishi.println(MSISDN+","+IP);
                         MSISDN=IP="";
                     }
                 }
@@ -75,7 +79,8 @@ public class FileProcessor{
                 }
             }
         }
+        System.out.println(sasa.toString()+": Exiting script...");
+        mwandishi.close();
     }
    
 }
-
