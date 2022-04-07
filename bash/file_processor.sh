3b025219a3c30b09ff646368ea94468d22e4f0d1
Maswi, MR <rmaswi360@outlook.com>
2022-04-07 19:31:53 +0300

    Style: commented unnecessary part of the fileprocessor.sh

--- bash/file_processor.sh
+++ bash/file_processor.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
 # Written by Maswi on Mar 3 2022
 
-EXPORTPATH="exports/export_`date +%Y%m%d%H%M%S`.csv"
+EXPORTPATH="exports/export_$(date +%Y%m%d%H%M%S).csv"
 MSISDN=""
 IP=""
-echo  "`date +%T_%F`:: Starting the script..."
-echo  "`date +%T_%F`:: Clearing the exort path if exists..."
+echo "$(date +%T_%F):: Starting the script..."
+echo "$(date +%T_%F):: Clearing the exort path if exists..."
 rm -rf $EXPORTPATH
-echo  "`date +%T_%F`:: Appending headers..."
-echo "MSISDN, IP_ADDRESS" > $EXPORTPATH
+echo "$(date +%T_%F):: Appending headers to the output file..."
+echo "MSISDN, IP_ADDRESS" >$EXPORTPATH
 
-echo  "`date +%T_%F`:: Pulling available files..."
+echo "$(date +%T_%F):: Pulling available files..."
 
-for file in files/*
-    do
-        if [ -f $file ]
-            then
-                echo  "`date +%T_%F`:: Processing $file"
-                while read line
-                    do
-                        if grep -q "^\<" <<< "$line" 
-                            then
-                                MSISDN=""
-                                IP=""
-                                echo 'Hapa tulifika...'
-                        fi
-                        if [[ -z $MSISDN ]]
-                            then
-                                echo $line | grep -o -E '^255\d{9,12}' | sort -u -f
-                                MSISDN=$(echo $line | grep -o -E '^255\d{9,12}' | sort -u -f)
-                                echo "Hata mi nilifika na MSISDN=$MSISDN"
-                        fi
-                        if [[ -z $IP ]] 
-                            then
-                                echo "Hata IP alifika.."
-                                IP=$(echo $line | grep -o -E '172\.27\.[0-9]+\.[0-9]+' | sort -u -f)
-                        fi
-                        if [[ -n $MSISDN && -n $IP ]]
-                            then
-                                echo $MSISDN, $IP >> $EXPORTPATH
-                                MSISDN=""
-                                IP=""
-                        fi
-                done < $file
-            exit
-        fi
-    done    
+SEPARATOR='<HGSDP'
+for file in files/*; do
+    if [ -f $file ]; then
+        echo "$(date +%T_%F):: Processing $file"
+        while read line; do
+            SUBSTR=$(echo $line | cut -c 1-6)
+            if [ "$SEPARATOR" =~ "$SUBSTR" ]; then
+                echo "Tumefika"
+                return
+            fi
+            # if [[ $line =~ .HGSDP. ]]; then
+            #     MSISDN=""
+            #     IP=""
+            #     echo 'Hapa tulifika...'
+            # fi
+            # if [[ -z $MSISDN ]]; then
+            #     echo $line | grep -o -E '^255\d{9,12}' | sort -u -f
+            #     MSISDN=$(echo $line | grep -o -E '^255\d{9,12}' | sort -u -f)
+            #     echo "Hata mi nilifika na MSISDN=$MSISDN"
+            # fi
+            # if [[ -z $IP ]]; then
+            #     echo "Hata IP alifika.."
+            #     IP=$(echo $line | grep -o -E '172\.27\.[0-9]+\.[0-9]+' | sort -u -f)
+            # fi
+            # if [[ -n $MSISDN && -n $IP ]]; then
+            #     echo $MSISDN, $IP >>$EXPORTPATH
+            #     MSISDN=""
+            #     IP=""
+            # fi
+        done <$file
+        exit
+    fi
+done
 
-echo  "`date +%T_%F`:: The output has been stored in $EXPORTPATH" 
+echo "$(date +%T_%F):: The output has been stored in $EXPORTPATH"
 
-echo  "`date +%T_%F`:: Exiting the script..." 
+echo "$(date +%T_%F):: Exiting the script..."
